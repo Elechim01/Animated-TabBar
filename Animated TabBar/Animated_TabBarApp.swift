ceff531cb38e6619aa81576f62f8c13d565e21ab
Elechim01 <mikymanny015@gmail.com>
2020-12-27 16:14:26 +0100

    Initial Commit

---
+++ Animated TabBar/Animated_TabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animated_TabBarApp.swift
+//  Animated TabBar
+//
+//  Created by Michele on 27/12/20.
+//
+
+import SwiftUI
+
+@main
+struct Animated_TabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Animated TabBar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Animated TabBar
+//
+//  Created by Michele on 27/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
